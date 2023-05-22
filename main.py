d2789f5c0b9576f11eb6c1939673d70bff248519
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-22 23:19:57 +0300

    Дз 21 травня

--- main.py
+++ main.py
@@ -1,71 +1,65 @@
-#Домашння робота
-class Clothing:
-    def __init__(self, material, name, price, size):
-        self.material = material
+class FoodItem:
+    def __init__(self, name, description, price):
         self.name = name
+        self.description = description
         self.price = price
-        self.size = size
-class Store:
-    def __init__(self, name, address):
-        self.name = name
-        self.address = address
-        self.inventory = []
+class Menu:
+    def __init__(self):
+        self.items = []
     def add_item(self, item):
-        self.inventory.append(item)
+        self.items.append(item)
     def remove_item(self, item):
-        self.inventory.remove(item)
-    def get_items(self):
-        return self.inventory
-    def search_items(self, keyword):
-        matching_items = []
-        for item in self.inventory:
-            if keyword.lower() in item.name.lower():
-                matching_items.append(item)
-        return matching_items
-class Customer:
-    def __init__(self, name, budget):
-        self.name = name
-        self.budget = budget
-        self.cart = []
-    def add_to_cart(self, item):
-        if item.price <= self.budget:
-            self.cart.append(item)
-            self.budget -= item.price
-            print(f"{item.name} додано в кошик..")
-        else:
-            print("Недостатньо бюджету, щоб додати товар у кошик.")
-    def remove_from_cart(self, item):
-        if item in self.cart:
-            self.cart.remove(item)
-            self.budget += item.price
-            print(f"{item.name} видалено з кошика.")
-        else:
-            print("Товар не знайдено в кошику.")
-    def view_cart(self):
-        print(f"{self.name} кошик:")
-        for item in self.cart:
-            print(f"- {item.name} (${item.price})")
-    def checkout(self):
-        total_price = sum(item.price for item in self.cart)
-        if total_price <= self.budget:
-            self.budget -= total_price
-            self.cart = []
-            print("Оформлення завершено.")
-        else:
-            print("Недостатній бюджет для завершення покупки.")
-shop = Store("Модний магазин", "123 Головна вул")
-item1 = Clothing("Бавовна", "Футболка", 20, "M")
-item2 = Clothing("Джинса", "Джинси", 50, "L")
-item3 = Clothing("Шерсть", "светр", 40, "S")
-shop.add_item(item1)
-shop.add_item(item2)
-shop.add_item(item3)
-customer = Customer("Максіка", 100)
-search_results = shop.search_items("джинси")
-if search_results:
-    customer.add_to_cart(search_results[0])
-customer.view_cart()
-customer.remove_from_cart(item1)
-customer.view_cart()
-customer.checkout()
-
+        self.items.remove(item)
+    def display_menu(self):
+        print("Menu:")
+        for item in self.items:
+            print(f"{item.name}: {item.description} - ${item.price}")
+class Order:
+    def __init__(self):
+        self.items = []
+    def add_item(self, item):
+        self.items.append(item)
+    def calculate_total(self):
+        total = 0
+        for item in self.items:
+            total += item.price
+        return total
+class Restaurant:
+    def __init__(self):
+        self.menu = Menu()
+        self.orders = []
+    def add_item_to_menu(self, item):
+        self.menu.add_item(item)
+    def remove_item_from_menu(self, item):
+        self.menu.remove_item(item)
+    def display_menu(self):
+        self.menu.display_menu()
+    def place_order(self, order):
+        self.orders.append(order)
+    def remove_order(self, order):
+        self.orders.remove(order)
+    def generate_bill(self, order):
+        total = order.calculate_total()
+        print("Bill:")
+        for item in order.items:
+            print(f"{item.name}: {item.price}")
+        print(f"Total: ${total}")
+food_item1 = FoodItem("Pizza", "Delicious pizza with cheese and toppings", 10)
+food_item2 = FoodItem("Burger", "Juicy burger with a side of fries", 8)
+food_item3 = FoodItem("Salad", "Fresh garden salad with dressing", 6)
+menu = Menu()
+menu.add_item(food_item1)
+menu.add_item(food_item2)
+menu.add_item(food_item3)
+menu.display_menu()
+order = Order()
+order.add_item(food_item1)
+order.add_item(food_item2)
+restaurant = Restaurant()
+restaurant.place_order(order)
+restaurant.generate_bill(order)
+with open("orders.txt", "w") as file:
+    file.write("Order:\n")
+    for item in order.items:
+        file.write(f"{item.name}: {item.price}\n")
+    file.write(f"Total: ${order.calculate_total()}")
